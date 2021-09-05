b31b3a13e9c231bf5a165fe8dc8008623e0778ac
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2021-09-05 23:21:41 +0200

    refactor(note): Make layout flexible for quotes

--- notes/note.py
+++ notes/note.py
@@ -1,48 +1,20 @@
 """Note module"""
+import copy
 
 import epdlib
 from PIL import Image
 
-layout1 = {
-    'quoteTitle': {
-        'type': 'TextBlock',
-        'image': None,
-        'max_lines': 1,
-        'padding': 1,
-        'width': 3 / 4,
-        'height': 1 / 6,
-        'abs_coordinates': (0, 0),
-        'hcenter': False,
-        'vcenter': True,
-        'relative': False,
-        'font': './notes/fonts/Roboto-Bold.ttf',
-        'font_size': None,
-        'mode': '1'
-    },
+layoutBaseQuoteFullWeather = {
     'quoteText': {
         'type': 'TextBlock',
         'image': None,
         'max_lines': 5,
         'padding': 1,
         'width': 3 / 4,
-        'height': 4 / 6,
-        'abs_coordinates': (0, None),
-        'relative': ['quoteText', 'quoteTitle'],
+        'height': 6 / 6,
+        'abs_coordinates': (0, 0),
+        'relative': False,
         'font': './notes/fonts/Roboto-LightItalic.ttf',
-        'font_size': None,
-        'mode': '1'
-    },
-
-    'quoteAuthor': {
-        'type': 'TextBlock',
-        'image': None,
-        'max_lines': 1,
-        'padding': 1,
-        'width': 3 / 4,
-        'height': 1 / 6,
-        'abs_coordinates': (0, None),
-        'relative': ['quoteAuthor', 'quoteText'],
-        'font': './notes/fonts/Roboto-Regular.ttf',
         'font_size': None,
         'mode': '1'
     },
@@ -56,7 +28,7 @@ layout1 = {
         'abs_coordinates': (None, 0),
         'hcenter': True,
         'vcenter': True,
-        'relative': ['quoteTitle', 'weatherIcon'],
+        'relative': ['quoteText', 'weatherIcon'],
         'mode': '1'
     },
 
@@ -68,12 +40,13 @@ layout1 = {
         'width': 1 / 4,
         'height': 1 / 6,
         'abs_coordinates': (None, None),
-        'relative': ['quoteTitle', 'weatherIcon'],
+        'relative': ['quoteText', 'weatherIcon'],
         'font': './notes/fonts/Roboto-Regular.ttf',
         'font_size': None,
         'mode': '1'
     },
 
+    # Also add direction somehow
     'weatherWindSpeed': {
         'type': 'TextBlock',
         'image': None,
@@ -82,7 +55,7 @@ layout1 = {
         'width': 1 / 4,
         'height': 1 / 6,
         'abs_coordinates': (None, None),
-        'relative': ['quoteTitle', 'weatherTemperature'],
+        'relative': ['quoteText', 'weatherTemperature'],
         'font': './notes/fonts/Roboto-Regular.ttf',
         'font_size': None,
         'mode': '1'
@@ -96,11 +69,41 @@ layout1 = {
         'width': 1 / 4,
         'height': 1 / 6,
         'abs_coordinates': (None, None),
-        'relative': ['quoteTitle', 'weatherWindSpeed'],
+        'relative': ['quoteText', 'weatherWindSpeed'],
         'font': './notes/fonts/Roboto-Regular.ttf',
         'font_size': None,
         'mode': '1'
     },
+}
+
+blockQuoteTitle = {
+    'type': 'TextBlock',
+    'image': None,
+    'max_lines': 1,
+    'padding': 1,
+    'width': 3 / 4,
+    'height': 1 / 6,
+    'abs_coordinates': (0, 0),
+    'hcenter': False,
+    'vcenter': True,
+    'relative': False,
+    'font': './notes/fonts/Roboto-Bold.ttf',
+    'font_size': None,
+    'mode': '1'
+}
+
+blockQuoteAuthor = {
+    'type': 'TextBlock',
+    'image': None,
+    'max_lines': 1,
+    'padding': 1,
+    'width': 3 / 4,
+    'height': 1 / 6,
+    'abs_coordinates': (0, None),
+    'relative': ['quoteAuthor', 'quoteText'],
+    'font': './notes/fonts/Roboto-Regular.ttf',
+    'font_size': None,
+    'mode': '1'
 }
 
 
@@ -108,10 +111,28 @@ class Note:
     """A Note to display that can be saved to disk"""
     def __init__(self):
         self.epdLayout = epdlib.Layout(resolution=(212, 104))
-        self.epdLayout.layout = layout1
 
     def update(self, updates):
         """Set data"""
+        theLayout = copy.deepcopy(layoutBaseQuoteFullWeather)
+        if 'quoteTitle' in updates and updates['quoteTitle'] is not None:
+            theLayout = {
+                'quoteTitle': copy.deepcopy(blockQuoteTitle)
+            }
+            theLayout.update(copy.deepcopy(layoutBaseQuoteFullWeather))
+            theLayout['quoteText']['abs_coordinates'] = (0, None)
+            theLayout['quoteText']['relative'] = ['quoteText', 'quoteTitle']
+
+        if 'quoteAuthor' in updates and updates['quoteAuthor'] is not None:
+            theLayout['quoteAuthor'] = copy.deepcopy(blockQuoteAuthor)
+
+        if ('quoteTitle' in updates and updates['quoteTitle'] is not None) != ('quoteAuthor' in updates and updates['quoteAuthor'] is not None):
+            theLayout['quoteText']['height'] = 5 / 6
+            theLayout['quoteText']['mac_lines'] = 6
+        elif 'quoteTitle' in updates and updates['quoteTitle'] is not None and 'quoteAuthor' in updates and updates['quoteAuthor'] is not None:
+            theLayout['quoteText']['height'] = 4 / 6
+
+        self.epdLayout.layout = theLayout
         self.epdLayout.update_contents(updates)
 
     def write(self, blackImagePath, colourImagePath):
@@ -119,9 +140,14 @@ class Note:
         blackImage = Image.new('L', self.epdLayout.resolution, 255)
         colourImage = Image.new('L', self.epdLayout.resolution, 255)
 
-        blackImage.paste(self.epdLayout.blocks['quoteTitle'].image, self.epdLayout.blocks['quoteTitle'].abs_coordinates)
+        if 'quoteTitle' in self.epdLayout.blocks:
+            blackImage.paste(self.epdLayout.blocks['quoteTitle'].image, self.epdLayout.blocks['quoteTitle'].abs_coordinates)
+
         blackImage.paste(self.epdLayout.blocks['quoteText'].image, self.epdLayout.blocks['quoteText'].abs_coordinates)
-        colourImage.paste(self.epdLayout.blocks['quoteAuthor'].image, self.epdLayout.blocks['quoteAuthor'].abs_coordinates)
+
+        if 'quoteAuthor' in self.epdLayout.blocks:
+            colourImage.paste(self.epdLayout.blocks['quoteAuthor'].image, self.epdLayout.blocks['quoteAuthor'].abs_coordinates)
+
         blackImage.paste(self.epdLayout.blocks['weatherIcon'].image, self.epdLayout.blocks['weatherIcon'].abs_coordinates)
         blackImage.paste(self.epdLayout.blocks['weatherTemperature'].image, self.epdLayout.blocks['weatherTemperature'].abs_coordinates)
         blackImage.paste(self.epdLayout.blocks['weatherWindSpeed'].image, self.epdLayout.blocks['weatherWindSpeed'].abs_coordinates)
